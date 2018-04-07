c1cd69f97539253501f56ce44d913ab160a0d0e3
Caio Muniz <caiocsmuniz1@gmail.com>
2018-04-06 21:59:26 -0300

    Add files via upload

---
+++ 1007.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+	double a, b, c, d, diff;
+	cin >> a >> b >> c >> d;
+	diff = (a*b) - (c*d);
+	printf("DIFERENCA = %.0f\n", diff);
+}
---
+++ 1009.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	char nome[1];
+	
+	double sal, venda;	
+	
+	cin >> nome;
+	
+	scanf("%lf\n%lf", &sal, &venda);
+	
+	printf("TOTAL = R$ %.2lf\n", sal+venda*0.15);
+}
